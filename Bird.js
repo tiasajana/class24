1318e61ba0a41d859411854e22990a0ad4c13364
tiasajana <74958834+tiasajana@users.noreply.github.com>
2020-12-06 14:51:42 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,27 @@
+class Bird {
+
+    constructor(x,y) {
+       var options={
+           restitution:0.8,
+           friction:1.0,
+           density:1.0
+       }
+       this.body=Bodies.rectangle(x,y,50,50,options)
+       this.width=50
+       this.height=50
+       World.add(world,this.body)
+    }
+    display(){
+     this.body.position.x = mouseX;
+     this.body.position.y = mouseY;
+
+    push()
+    translate(this.body.position.x,this.body.position.y)
+    rotate(this.body.angle)
+    stroke("purple")
+    fill("red")
+    rectMode(CENTER)
+    rect(0,0,this.width,this.height)
+    pop()
+    }
+}
